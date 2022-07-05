a75cc36db255efcd4accba374704e3f7782dd658
kikigigi <momokocute1@gmail.com>
2022-07-05 11:51:33 +0100

    Fix uswgi.ini

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, Items
 from resources.store import Store, Stores
-from db import db
 from dotenv import load_dotenv
 import os
 
@@ -17,10 +16,6 @@ app.secret_key = os.getenv('SECRET_KEY')
 api = Api(app)
 app.config['JWT_AUTH_URL_RULE'] = '/login'
 
-@app.before_request
-def create_tables():
-    db.create_all()
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, '/item/<string:name>')
@@ -30,5 +25,6 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Stores, '/stores')
 
 if __name__ == '__main__':
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
